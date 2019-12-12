79a5edff3225eaa010ef5b090b0d4abd8fddbd47
Jerry <jerrybzhu@gmail.com>
2019-12-12 21:41:39 +1100

    added tasklist and task components + styling

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import Column from './column';
 import ReactDOM from 'react-dom';
 import initialData from './initial-data';
+import '@atlaskit/css-reset';
 
 class App extends React.Component {
     state = initialData;
 
     render() {
         return this.state.columnOrder.map( columnId => {
-            const column = this.state.columns(columnId);
+            const column = this.state.columns[columnId];
             const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
 
-            return <Columns key={column.id} tasks={tasks} />
+            return <Column key={column.id} tasks={tasks} column={column}/>
         })
     }
 }
