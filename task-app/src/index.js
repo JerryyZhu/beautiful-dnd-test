00ef8095d6f7aa6dcb781d5da560f6618713da8f
Jerry <jerrybzhu@gmail.com>
2019-12-12 21:16:30 +1100

    Boilerplate + initialData

---
+++ task-app/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import initialData from './initial-data';
+
+class App extends React.Component {
+    state = initialData;
+
+    render() {
+        return this.state.columnOrder.map( columnId => {
+            const column = this.state.columns(columnId);
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return <Columns key={column.id} tasks={tasks} />
+        })
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
